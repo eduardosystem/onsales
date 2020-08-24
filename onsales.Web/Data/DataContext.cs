1817fa9894514ffb1a740a24c5a8265ab40ec97f
Eduardo David Ruiz Pinche <eduardosysrp@hotmail.com>
2020-08-24 01:21:25 -0500

    Codigo inicial con algnos errores en el index, para modificar. js. eliminar.

---
+++ onsales.Web/Data/DataContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using onsales.Common.Entities;
+
+namespace onsales.Web.Data
+{
+    public class DataContext : DbContext
+    {
+        //Constructor para la conexion a la base de datos
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        //Mapeamos las tablas
+        public DbSet<Country> Countries { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+
+    }
+
+
+}
